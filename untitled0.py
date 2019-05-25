1af12673c6b5a595adea21f92668f905e2f04911
Justus Will <justuswill@gmail.com>
2019-05-25 18:23:38 +0200

    Test of Repository

---
+++ untitled0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def getMotor(dx, dy, dphi):
+    '''
+    Caculate the activaion of the four mecanum wheels using the deired change in x,y,phi
+    '''
+    v1 = 1
+    v2 = 1
+    v3 = 1
+    v4 = 1
+    return (v1, v2, v3, v4)
+
